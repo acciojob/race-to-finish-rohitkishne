649c38f6664ddb0fafdf5b7e8d8d3f8130a820f2
Rohit kishne <92744319+rohitkishne@users.noreply.github.com>
2023-07-02 18:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 window.promises = [];
 
+let a = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("output")
+    }, 1000);
+})
+let b = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("output")
+    }, 2000);
+})
+let c = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("output")
+    }, 3000);
+})
+let d = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("output")
+    }, 4000);
+})
+let e = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("output")
+    }, 5000);
+})
+
+let ans = Promise.any([a,b,c,d,e])
+
+ans.then(data => {
+	return data;
+})
+
 // Do not change the code above this
 // add your promises to the array `promises`
